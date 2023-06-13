6f35e1f3a02c588cd778694b6761da9929deadef
john-330 <jaxon330@gmail.com>
2023-06-13 12:32:36 -0500

    zipper list problem solved

---
+++ Linked_List/zipperLists.js
@@ -0,0 +1,83 @@
+//* zipper Lists
+/*
+Write a function, zipperLists, that takes in the head of two linked lists as arguments. 
+The function should zipper the two lists together into single linked list by alternating nodes. 
+If one of the linked lists is longer than the other, the resulting list should terminate with the remaining nodes. 
+The function should return the head of the zippered linked list.
+
+Do this in-place, by mutating the original Nodes.
+
+You may assume that both input lists are non-empty.
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+a.next = b;
+b.next = c;
+// a -> b -> c
+
+const x = new Node("x");
+const y = new Node("y");
+const z = new Node("z");
+x.next = y;
+y.next = z;
+// x -> y -> z
+
+//! Iterative solution
+const zipperLists = (head1, head2) => {
+    let head = head1;
+    let current1 = head1.next;
+    let current2 = head2;
+    let count = 0;
+    while(current1 !== null && current2 !== null) {
+        if(count % 2 === 0) {
+            head.next = current2;
+            current2 = current2.next
+        } else {
+            head.next = current1;
+            current1 = current1.next
+        }
+
+        head = head.next;
+        count += 1;
+    }
+    
+    if (current1 !== null) head.next = current1;
+    if (current2 !== null) head.next = current2;
+
+    return head
+}
+//? n = # of node1
+//? m = # of node2
+//? Time: O(min(n,m))
+//? Space: O(1)
+
+//! Recursive solution
+const zipperLists2 = (head1, head2) => {
+    if(head1 === null && head2 === null) return null;
+    if(head1 === null) return head2;
+    if(head2 === null) return head1;
+    let next1 = head1.next;
+    let next2 = head2.next;
+    head1.next = head2;
+    head2.next = zipperLists2(next1, next2);
+    return head1
+}
+
+//? n = # of node1
+//? m = # of node2
+//? Time: O(min(n,m))
+//? Space: O(min(n,m))
+
+
+zipperLists(a, x);
+zipperLists2(a, x);
+// a -> x -> b -> y -> c -> z
