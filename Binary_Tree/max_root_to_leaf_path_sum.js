e455f919c67fe223d1bf37d0011f6ef19043e86b
john-330 <jaxon330@gmail.com>
2023-07-06 09:38:54 -0500

    javascript solution for max root to leaf path sum problem

---
+++ Binary_Tree/max_root_to_leaf_path_sum.js
@@ -0,0 +1,40 @@
+//* max root to leaf path sum
+/*
+Write a function, maxPathSum, that takes in the root of a binary tree that contains number values. 
+The function should return the maximum sum of any root to leaf path within the tree.
+
+You may assume that the input tree is non-empty.
+*/
+
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  const a = new Node(3);
+  const b = new Node(11);
+  const c = new Node(4);
+  const d = new Node(4);
+  const e = new Node(-2);
+  const f = new Node(1);
+  
+  a.left = b;
+  a.right = c;
+  b.left = d;
+  b.right = e;
+  c.right = f;
+  
+  //! Recursive solution
+  const maxPathSum = (root) => {
+    if(root === null) return -Infinity;
+    if(root.left === null && root.right === null) return root.val;
+    const maxValuePath = Math.max(maxPathSum(root.right), maxPathSum(root.left));
+    return root.val + maxValuePath;
+  };
+  
+  //? n = number of nodes
+  //? Time: O(n)
+  //? Space: O(n)
